ca06c88c0de538c4a9ce36542cb3c3773928389d
agalal <agalal@a25960e1-073d-4d9f-9c09-2c24de24fe4d>
2011-03-26 17:16:45 +0000

    - Disabling cam_index in CamOptions GUI for JMyron Library - renaming "processors" pkg to "filters"

--- src/modules/FileCargo.java
+++
@@ -1,6 +0,0 @@
-/*
- * package modules; public class FileCargo extends Cargo { private String[]
- * arr_header_data; public FileCargo(String[] tags) { super(tags); } public
- * String[] getHeaderInfo() { return arr_header_data; } public void
- * setHeaderInfo(String[] hi) { arr_header_data=hi; } }
- */
