c76952800d7d471a3537dd1e41f54012fbe867b9
agalal <agalal@a25960e1-073d-4d9f-9c09-2c24de24fe4d>
2011-09-30 17:58:00 +0000

    - adding copyright

--- src/utils/video/input/VideoFileModule2.java
+++ src/utils/video/input/VideoFileModule2.java
@@ -26,55 +26,80 @@
  */
 package utils.video.input;
 
-import java.awt.Point;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
 
-import jagvidlib.JAGVidLib;
+import javax.media.Processor;
+
+import org.eclipse.swt.widgets.Widget;
+
 import utils.video.FrameIntArray;
+import utils.video.ImageManipulator;
+
+import com.sun.media.protocol.avi.DataSource;
+
+import de.humatic.dsj.DSFiltergraph;
+import de.humatic.dsj.DSMovie;
 
 /**
  * @author Creative
  *
  */
-public class VideoFileModule implements VidInputter
+public class VideoFileModule2 implements VidInputter
 {
+
+	private Processor proc;
+	private DataSource data_src;
+	private JMFGrabber ana_eff = null;
 	private boolean stop_stream;
 	private FrameIntArray fia;
 	private int status;
 	int[] data;
-
-	private JAGVidLib vidLib;
-
 	private class RunnableAGCamLib implements Runnable
 	{
+		boolean sett;
+
 		@Override
 		public void run()
 		{
-			Point dims = vidLib.getVideoDimensions();
-			fia.frame_data = new int[dims.x*dims.y];
+			fia.frame_data = new int[640*480];
 			try
 			{
 				Thread.sleep(1000);
 			} catch (InterruptedException e1)
 			{
+				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			vidLib.play();
+			movie.play();
+			//movie.pause();
 			while (!stop_stream)
 			{
 				//movie.play();
 				try
 				{
+
 					Thread.sleep(30);
 				} catch (final InterruptedException e)
 				{
 					e.printStackTrace();
 				}
-				//long l1 = System.currentTimeMillis();
-				//data= ImageManipulator.byteRGB2IntRGB(((DataBufferByte)movie.getImage().getRaster().getDataBuffer()).getData());
-				fia.frame_data= vidLib.getCurrentFrameInt();
-				//long l2 = System.currentTimeMillis();
+				//movie.step(1);
+				long l1 = System.currentTimeMillis();
+				//if(!sett)
+				{
+					//data= ImageManipulator.byteRGB2IntRGB(((DataBufferByte)movie.getImage().getRaster().getDataBuffer()).getData());
+					fia.frame_data= ImageManipulator.byteRGB2IntRGB(movie.getData());
+					sett=true;
+				}
+/*				else
+				{
+					System.arraycopy(data, 0, fia.frame_data , 0, data.length);
+				}*/
+				//getRGB(0, 0, 640, 480, null, 0, 640);
+				long l2 = System.currentTimeMillis();
 				status = 1;
-				//System.out.println(l2-l1 + "\n");
+				System.out.println(l2-l1 + "\n");
 			}
 		}
 
@@ -82,11 +107,8 @@ public class VideoFileModule implements VidInputter
 	/**
 	 * 
 	 */
-	public VideoFileModule(String file)
-	{
-		vidLib = new JAGVidLib();
-		vidLib.initialize(file);
-	}
+	public VideoFileModule2()
+	{}
 
 	/* (non-Javadoc)
 	 * @see utils.video.input.VidInputter#displayMoreSettings()
@@ -94,6 +116,7 @@ public class VideoFileModule implements VidInputter
 	@Override
 	public int displayMoreSettings()
 	{
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
@@ -103,6 +126,7 @@ public class VideoFileModule implements VidInputter
 	@Override
 	public String getName()
 	{
+		// TODO Auto-generated method stub
 		return "VideoFileReader";
 	}
 
@@ -112,6 +136,7 @@ public class VideoFileModule implements VidInputter
 	@Override
 	public int getNumCams()
 	{
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
@@ -121,8 +146,10 @@ public class VideoFileModule implements VidInputter
 	@Override
 	public int getStatus()
 	{
+		// TODO Auto-generated method stub
 		return status;
 	}
+	DSMovie movie;
 	private Thread th_update_image;
 	/* (non-Javadoc)
 	 * @see utils.video.input.VidInputter#initialize(utils.video.FrameIntArray, int, int, int)
@@ -131,8 +158,77 @@ public class VideoFileModule implements VidInputter
 	public boolean initialize(FrameIntArray frameData, int width, int height, int camIndex)
 	{
 		fia = frameData;
+		//movie = new DSMovie("video.avi", DSFiltergraph.DD7, null);
+		movie = new DSMovie("video.avi", DSFiltergraph.DD7, null);
+
+
+
 		return true;
-	}
+		/*
+
+		data_src = new DataSource();
+		try
+		{
+			width=640;
+			height=272;
+			data_src.setLocator(new MediaLocator(new URL("file:\\E:\\jmj-g4ce.avi")));
+			data_src.connect();
+			proc = Manager.createProcessor(data_src);
+
+			proc.configure();
+			while (proc.getState() != Processor.Configured)
+				Thread.sleep(10);
+			final TrackControl[] tracks = proc.getTrackControls();
+
+			ana_eff = new JMFGrabber(
+					width,
+					height,
+					frameData);
+			ana_eff.setInputFormat(tracks[0].getFormat());
+			ana_eff.setOutputFormat(new RGBFormat(new Dimension(width, height), width
+		 * height
+		 * 3, Format.byteArray, 30.0f, 12, 3, 2, 1, 3, width * 3, 0, 0));
+
+			final Codec[] codecs = { ana_eff };
+			try
+			{
+				tracks[0].setCodecChain(codecs);
+			} catch (final UnsupportedPlugInException e)
+			{
+				e.printStackTrace();
+			}
+
+
+
+			proc.realize();
+			while (proc.getState() != Controller.Realized)
+				Thread.sleep(10);
+
+
+			proc.start();
+			while (proc.getState() != Controller.Started)
+				Thread.sleep(10);
+
+
+		} catch (MalformedURLException e)
+		{
+			e.printStackTrace();
+		} catch (NoProcessorException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+		 */}
 
 	/* (non-Javadoc)
 	 * @see utils.video.input.VidInputter#setFormat(java.lang.String)
@@ -140,6 +236,7 @@ public class VideoFileModule implements VidInputter
 	@Override
 	public void setFormat(String format)
 	{
+		// TODO Auto-generated method stub
 
 	}
 
@@ -163,7 +260,8 @@ public class VideoFileModule implements VidInputter
 	public void stopModule()
 	{
 		stop_stream=true;
-		vidLib.stop();
+		movie.stop();
+
 	}
 
 }
