1a11f79f29780e36c24299de349e7e9a05b47ac2
agalal <agalal@a25960e1-073d-4d9f-9c09-2c24de24fe4d>
2013-02-01 21:29:39 +0000

    ASSIGNED - # 306: Establish Jenkins CI for testing http://agalaldev.info/dev/mantis/view.php?id=306

--- trunk/ants/test/runGUITests.bat
+++ trunk/ants/test/runGUITests.bat
@@ -1,1 +1,7 @@
-ant BMTTestGUI -f %BMT_WS%\BMT\ants\test\regressGUI.xml -Dbinloctmp=%BMT_WS%\BMT\output\release\*.jar -Declipse.home=%ECLIPSE_HOME% -Dbasedir=%BMT_WS%\BMT
+:: remove bin dir to prevent BMT from launching from it
+rmdir %BMT_WS%\BMT\bin /S /Q
+ant BMTTestGUI -f %BMT_WS%\BMT\ants\test\regressGUI.xml ^
+-Dbinloctmp.tests=%tests_bin% ^
+-Dbinloctmp=%BMT_WS%\BMT\output\release\*.jar ^
+-Declipse.home=%ECLIPSE_HOME% ^
+-Dbasedir=%BMT_WS%\BMT
