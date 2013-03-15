2f4d869dd8ef2b7de69080474b2d0c2ba3659507
agalal <agalal@a25960e1-073d-4d9f-9c09-2c24de24fe4d>
2013-03-15 17:32:29 +0000

    RESOLVED - # 333: Improving UI for FilterGraph http://agalaldev.info/dev/mantis/view.php?id=333

--- branches/b_bmt_0.6_0000279/src/ui/filtergraph/FilterGraph.java
+++ branches/b_bmt_0.6_0000279/src/ui/filtergraph/FilterGraph.java
@@ -7,6 +7,8 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -26,7 +28,6 @@ import org.eclipse.zest.core.widgets.ZestStyles;
 import org.eclipse.zest.layouts.LayoutStyles;
 import org.eclipse.zest.layouts.algorithms.HorizontalTreeLayoutAlgorithm;
 
-import ui.box.InputBox;
 import utils.PManager;
 import filters.FiltersConnectionRequirements;
 import filters.FiltersNamesRequirements;
@@ -36,44 +37,47 @@ import filters.FiltersSetup;
 
 public class FilterGraph {
 
+	public static class Filter {
+		private final FilterRequirement filterRequirement;
+
+		public Filter(final String name, final String id,
+				final FilterTrigger trigger) {
+			filterRequirement = new FilterRequirement(name, id, trigger);
+			setName(name);
+			setId(id);
+		}
+
+		public String getId() {
+			return filterRequirement.getID();
+		}
+
+		public String getName() {
+			return filterRequirement.getName();
+		}
+
+		public FilterTrigger getTrigger() {
+			return filterRequirement.getTrigger();
+		}
+
+		public void setId(final String id) {
+			filterRequirement.setID(id);
+		}
+
+		public void setName(final String name) {
+			filterRequirement.setName(name);
+		}
+
+		public void setTrigger(final FilterTrigger trigger) {
+			filterRequirement.setTrigger(trigger);
+		}
+	}
+
 	private static FilterGraph self;
 
 	public static FilterGraph getDefault() {
 		if (self == null)
 			self = new FilterGraph();
 		return self;
-	}
-	
-	private static class Filter{
-		private FilterRequirement filterRequirement;
-		private int numberInPorts=0;
-		private int numberOutPorts=0;
-		public void setName(String name) {
-			filterRequirement.setName(name);
-		}
-		public String getName() {
-			return filterRequirement.getName();
-		}
-		public void setId(String id) {
-			filterRequirement.setID(id);
-		}
-		public String getId() {
-			return filterRequirement.getID();
-		}
-		
-		public FilterTrigger getTrigger(){
-			return filterRequirement.getTrigger();
-		}
-		
-		public void setTrigger(FilterTrigger trigger){
-			filterRequirement.setTrigger(trigger);
-		}
-		
-		public Filter(String name,String id, FilterTrigger trigger) {
-			filterRequirement=new FilterRequirement(name, id, trigger);
-			setName(name);
-			setId(id);
-		}
 	}
 
 	private boolean addingLink;
@@ -83,7 +87,7 @@ public class FilterGraph {
 	private FiltersSetup filtersSetup;
 	private Graph graph;
 
-	private HashMap<Filter,GraphNode> nodes;
+	private HashMap<Filter, GraphNode> nodes;
 	private Shell shell;
 	private GraphNode srcNode;
 
@@ -110,6 +114,12 @@ public class FilterGraph {
 		initializeForm(parent);
 	}
 
+	public void addNewNode(final Filter filter) {
+		final GraphNode node = new GraphNode(graph, SWT.NULL, filter.getName());
+		nodes.put(filter, node);
+		layout();
+	}
+
 	private void clearGraph() {
 		if (nodes != null) {
 			disposeAllNodes(nodes.values());
@@ -121,6 +131,17 @@ public class FilterGraph {
 			w.dispose();
 		}
 		nodes.clear();
+	}
+
+	private Object getKey(final Object value, final HashMap<?, ?> map) {
+		Object key = null;
+		for (final Object tmpKey : map.keySet()) {
+			if (map.get(tmpKey) == value) {
+				key = tmpKey;
+				break;
+			}
+		}
+		return key;
 	}
 
 	private GraphNode getNode(final String name) {
@@ -152,12 +173,14 @@ public class FilterGraph {
 					if (selection.size() == 1) {
 						final Object selectedItem = selection.get(0);
 						if (selectedItem instanceof GraphNode) {
-							GraphNode selectedNode = (GraphNode) selectedItem;
+							final GraphNode selectedNode = (GraphNode) selectedItem;
 							if (srcNode == null)
 								srcNode = selectedNode;
 							else {
-								new GraphConnection(graph, ZestStyles.CONNECTIONS_DIRECTED, srcNode, selectedNode);
-								
+								new GraphConnection(graph,
+										ZestStyles.CONNECTIONS_DIRECTED,
+										srcNode, selectedNode);
+
 								System.out
 										.println("a new link is added with:\nsrc: "
 												+ srcNode
@@ -180,77 +203,56 @@ public class FilterGraph {
 			}
 		});
 
-		final Button btnDelete = new Button(graph, SWT.NONE);
-		btnDelete.addSelectionListener(new SelectionAdapter() {
+		graph.addKeyListener(new KeyListener() {
+
 			@Override
-			public void widgetSelected(final SelectionEvent e) {
-				final List<?> selection = graph.getSelection();
-				final ArrayList<GraphNode> nodesToRemove = new ArrayList<GraphNode>();
-				final ArrayList<GraphConnection> connectionsToRemove = new ArrayList<GraphConnection>();
-				for (final Object obj : selection) {
-					if (obj instanceof GraphNode) {
-						nodesToRemove.add((GraphNode) obj);
+			public void keyPressed(final KeyEvent arg0) {
+				// TODO Auto-generated method stub
 
-					} else if (obj instanceof GraphConnection) {
-						connectionsToRemove.add((GraphConnection) obj);
+			}
+
+			@Override
+			public void keyReleased(final KeyEvent arg0) {
+				if (arg0.keyCode == SWT.DEL) {
+					final List<?> selection = graph.getSelection();
+					final ArrayList<GraphNode> nodesToRemove = new ArrayList<GraphNode>();
+					final ArrayList<GraphConnection> connectionsToRemove = new ArrayList<GraphConnection>();
+					for (final Object obj : selection) {
+						if (obj instanceof GraphNode) {
+							nodesToRemove.add((GraphNode) obj);
+
+						} else if (obj instanceof GraphConnection) {
+							connectionsToRemove.add((GraphConnection) obj);
+						}
 					}
+
+					/*
+					 * we split the removal process to prevent the
+					 * ConcurrentModificationException in the SWT thread
+					 */
+					for (final GraphConnection graphConnection : connectionsToRemove)
+						removeConnection(graphConnection);
+
+					for (final GraphNode graphNode : nodesToRemove)
+						removeNode(graphNode);
+
+					selection.clear();
 				}
-
-				/*
-				 * we split the removal process to prevent the
-				 * ConcurrentModificationException in the SWT thread
-				 */
-				for (final GraphConnection graphConnection : connectionsToRemove)
-					removeConnection(graphConnection);
-
-				for (final GraphNode graphNode : nodesToRemove)
-					removeNode(graphNode);
-
-				selection.clear();
 			}
 		});
-		btnDelete.setBounds(539, 407, 75, 25);
-		btnDelete.setText("delete");
 
 		btnNewNode = new Button(graph, SWT.NONE);
 		btnNewNode.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(final SelectionEvent e) {
-				InputBox boxID = new InputBox(shell, "Filter ID", "Please enter new filter's ID", true);
-				String id = boxID.show();
-				if(id!=null){
-					InputBox boxName = new InputBox(shell, "Filter Name", "Please enter new filter's name", true);
-					String name = boxName.show();
-					if(name!=null){
-						InputBox triggerBox = new InputBox(shell, "Filter Trigger", "Please enter new filter's trigger type", true);
-						String trigger = triggerBox.show();
-						if(trigger!=null){
-							
-							FilterTrigger filterTrigger=null;
-							if(trigger.equals("manual"))
-								filterTrigger=FilterTrigger.MANUAL;
-							else if(trigger.equals("processing"))
-								filterTrigger=FilterTrigger.PROCESSING;
-							else if(trigger.equals("streaming"))
-								filterTrigger=FilterTrigger.STREAMING;
-							
-							Filter filter = new Filter(name, id,filterTrigger);
-							GraphNode node = new GraphNode(graph, SWT.NULL,name);
-							nodes.put(filter , node);
-							layout();
-						}else{
-							// TODO: implement cancel
-						}
-					}else{
-						// TODO: implement cancel
-					}
-				}else{
-					// TODO: implement cancel
-				}
+				FilterNodeProperties.getDefault().show();
+				FilterNodeProperties.getDefault().setFilterIDs(
+						PManager.getDefault().getVideoManager()
+								.getFilterManager().getFiltersIDs());
 			}
 		});
 		btnNewNode.setBounds(539, 376, 75, 25);
-		btnNewNode.setText("add node");
+		btnNewNode.setText("Add Node");
 
 		btnNewLink = new Button(graph, SWT.NONE);
 		btnNewLink.addSelectionListener(new SelectionAdapter() {
@@ -261,17 +263,17 @@ public class FilterGraph {
 			}
 		});
 		btnNewLink.setBounds(539, 345, 75, 25);
-		btnNewLink.setText("add link");
-		
-		Button btnApply = new Button(graph, SWT.NONE);
+		btnNewLink.setText("Add Link");
+
+		final Button btnApply = new Button(graph, SWT.NONE);
 		btnApply.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(final SelectionEvent e) {
 				saveToFiltersSetup();
 			}
 		});
-		btnApply.setBounds(539, 314, 75, 25);
-		btnApply.setText("apply");
+		btnApply.setBounds(539, 407, 75, 25);
+		btnApply.setText("Apply");
 
 		layout();
 	}
@@ -293,37 +295,30 @@ public class FilterGraph {
 		node.dispose();
 		nodes.remove(getKey(node, nodes));
 	}
-	
-	private Object getKey(Object value,HashMap<?,?> map){
-		Object key=null;
-		for(Object tmpKey:map.keySet()){
-			if(map.get(tmpKey)==value){
-				key=tmpKey;
-				break;
-			}
-		}
-		return key;
-	}
-	
+
 	/**
 	 * Saves the current filters' connections into the FiltersSetup instance.
 	 */
 	@SuppressWarnings("unchecked")
-	private void saveToFiltersSetup(){
+	private void saveToFiltersSetup() {
 		filtersSetup.getFiltersNamesRequirements().clearFilterNames();
-		for(Filter filter:nodes.keySet()){
-			filtersSetup.getFiltersNamesRequirements().addFilter(filter.getName(), filter.getId(),filter.getTrigger());
+		for (final Filter filter : nodes.keySet()) {
+			filtersSetup.getFiltersNamesRequirements().addFilter(
+					filter.getName(), filter.getId(), filter.getTrigger());
 		}
-		
+
 		filtersSetup.getConnectionRequirements().clearConnections();
-		
-		for(GraphConnection connection:(List<GraphConnection>)graph.getConnections()){
-			filtersSetup.getConnectionRequirements().connectFilters(connection.getSource().getText(), connection.getDestination().getText());	
+
+		for (final GraphConnection connection : (List<GraphConnection>) graph
+				.getConnections()) {
+			filtersSetup.getConnectionRequirements().connectFilters(
+					connection.getSource().getText(),
+					connection.getDestination().getText());
 		}
-		
+
 		PManager.getDefault().getVideoManager().signalFiltersSetupChange();
 	}
-	
+
 	public void setFilterSetup(final FiltersSetup filtersSetup) {
 		this.filtersSetup = filtersSetup;
 		final FiltersConnectionRequirements connectionRequirements = filtersSetup
@@ -333,14 +328,17 @@ public class FilterGraph {
 
 		clearGraph();
 
-		nodes = new HashMap<Filter,GraphNode>();
+		nodes = new HashMap<Filter, GraphNode>();
 
 		for (final Iterator<FilterRequirement> it = filtersNamesRequirements
 				.getFilters(); it.hasNext();) {
 			final FilterRequirement filterRequirement = it.next();
-			String filterName = filterRequirement.getName();
-			String filterID = filterRequirement.getID();
-			nodes.put(new Filter(filterName, filterID,filterRequirement.getTrigger()), new GraphNode(graph, SWT.NULL, filterName));
+			final String filterName = filterRequirement.getName();
+			final String filterID = filterRequirement.getID();
+			nodes.put(
+					new Filter(filterName, filterID, filterRequirement
+							.getTrigger()), new GraphNode(graph, SWT.NULL,
+							filterName));
 		}
 
 		final ArrayList<String[]> connectionsReq = connectionRequirements
@@ -349,9 +347,8 @@ public class FilterGraph {
 		for (final String[] pair : connectionsReq) {
 			final String srcName = pair[0];
 			final String dstName = pair[1];
-			new GraphConnection(graph,
-					ZestStyles.CONNECTIONS_DIRECTED, getNode(srcName),
-					getNode(dstName));
+			new GraphConnection(graph, ZestStyles.CONNECTIONS_DIRECTED,
+					getNode(srcName), getNode(dstName));
 		}
 
 		layout();
---
+++ branches/b_bmt_0.6_0000279/src/ui/filtergraph/FilterNodeProperties.java
@@ -0,0 +1,154 @@
+/**
+ * 
+ */
+package ui.filtergraph;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import ui.filtergraph.FilterGraph.Filter;
+import filters.FiltersNamesRequirements.FilterTrigger;
+
+/**
+ * @author Creative
+ * 
+ */
+public class FilterNodeProperties {
+	private static FilterNodeProperties self;
+
+	public static FilterNodeProperties getDefault() {
+		if (self == null)
+			self = new FilterNodeProperties();
+		return self;
+	}
+
+	private final Combo combo;
+
+	private final List list;
+	private final Shell shell;
+	private final Text text;
+
+	public FilterNodeProperties() {
+		shell = new Shell(SWT.CLOSE);
+		shell.setSize(284, 309);
+		shell.addListener(SWT.Close, new Listener() {
+
+			@Override
+			public void handleEvent(final Event arg0) {
+				hide();
+				arg0.doit = false;
+			}
+		});
+		shell.setLayout(new FormLayout());
+
+		final Group grpFilterProperties = new Group(shell, SWT.NONE);
+		final FormData fd_grpFilterProperties = new FormData();
+		fd_grpFilterProperties.right = new FormAttachment(0, 274);
+		fd_grpFilterProperties.top = new FormAttachment(0, 5);
+		fd_grpFilterProperties.left = new FormAttachment(0, 5);
+		grpFilterProperties.setLayoutData(fd_grpFilterProperties);
+		grpFilterProperties.setLayout(new GridLayout(2, false));
+		grpFilterProperties.setText("Filter Properties");
+
+		final Label lblName = new Label(grpFilterProperties, SWT.NONE);
+		lblName.setText("Name");
+
+		text = new Text(grpFilterProperties, SWT.BORDER);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+
+		final Label lblId = new Label(grpFilterProperties, SWT.NONE);
+		lblId.setText("ID");
+
+		list = new List(grpFilterProperties, SWT.BORDER | SWT.V_SCROLL);
+		final GridData gd_list = new GridData(SWT.FILL, SWT.CENTER, false,
+				false, 1, 1);
+		gd_list.heightHint = 152;
+		list.setLayoutData(gd_list);
+
+		final Label lblTrigger = new Label(grpFilterProperties, SWT.NONE);
+		lblTrigger.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false,
+				false, 1, 1));
+		lblTrigger.setText("Trigger");
+
+		combo = new Combo(grpFilterProperties, SWT.READ_ONLY);
+		combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1,
+				1));
+
+		final Button btnCancel = new Button(shell, SWT.NONE);
+		btnCancel.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				hide();
+			}
+		});
+		final FormData fd_btnCancel = new FormData();
+		fd_btnCancel.right = new FormAttachment(0, 180);
+		fd_btnCancel.top = new FormAttachment(0, 249);
+		fd_btnCancel.left = new FormAttachment(0, 92);
+		btnCancel.setLayoutData(fd_btnCancel);
+		btnCancel.setText("Cancel");
+
+		final Button btnOk = new Button(shell, SWT.NONE);
+		final FormData fd_btnOk = new FormData();
+		fd_btnOk.right = new FormAttachment(0, 274);
+		fd_btnOk.top = new FormAttachment(0, 248);
+		fd_btnOk.left = new FormAttachment(0, 186);
+		btnOk.setLayoutData(fd_btnOk);
+		btnOk.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+
+				final String id = list.getSelection()[0];
+				final String name = text.getText();
+				final String trigger = combo.getText();
+
+				final FilterTrigger filterTrigger = FilterTrigger
+						.valueOf(trigger);
+				final Filter filter = new Filter(name, id, filterTrigger);
+				FilterGraph.getDefault().addNewNode(filter);
+
+				// hide dialog
+				hide();
+			}
+		});
+		btnOk.setText("OK");
+	}
+
+	private void hide() {
+		shell.setVisible(false);
+	}
+
+	public void setFilterIDs(final String[] ids) {
+		list.removeAll();
+		for (final String id : ids) {
+			list.add(id);
+		}
+	}
+
+	public void show() {
+		shell.open();
+		text.setText("");
+
+		// refresh filter triggers
+		combo.removeAll();
+		for (final FilterTrigger trigger : FilterTrigger.values()) {
+			combo.add(trigger.toString());
+		}
+		combo.setText(combo.getItem(0));
+	}
+}
