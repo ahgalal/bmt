0d38f9c122c4aac848fca9cac4be820bed845519
agalal <ahgalal@users.noreply.github.com>
2013-05-22 23:21:57 +0000

    Merged revision(s) 362-363 from branches/b_bmt_0.6_0000279:

--- ants/test/runGUITests.sh
+++ ants/test/runGUITests.sh
@@ -1,7 +1,7 @@
 export BASE_DIR=$BMT_WS/BMT/output/release
 cp -rf $BMT_WS/BMT/ants/test $BASE_DIR/ants/
-ant BMTTestGUI -f $BASE_DIR/ants/test/regressGUI.xml ^
--Declipse.home=$ECLIPSE_HOME ^
--Dtests.jar=$BASE_DIR/tests.jar ^
--Dsrc.jar=$BASE_DIR/BMT_v0.6.jar ^
+ant BMTTestGUI -f $BASE_DIR/ants/test/regressGUI.xml \
+-Declipse.home=$ECLIPSE_HOME \
+-Dtests.jar=$BASE_DIR/tests.jar \
+-Dsrc.jar=$BASE_DIR/BMT_v0.6.jar \
 -Dbasedir=$BASE_DIR
