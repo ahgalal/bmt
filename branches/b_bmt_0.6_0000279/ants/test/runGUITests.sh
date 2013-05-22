40edfcb84f5e31d4472dd4318e3c6559ea9b6ef6
agalal <agalal@a25960e1-073d-4d9f-9c09-2c24de24fe4d>
2013-05-22 23:18:40 +0000

    Saving script in Unix format.

--- branches/b_bmt_0.6_0000279/ants/test/runGUITests.sh
+++ branches/b_bmt_0.6_0000279/ants/test/runGUITests.sh
@@ -1,6 +1,5 @@
 export BASE_DIR=$BMT_WS/BMT/output/release
 cp -rf $BMT_WS/BMT/ants/test $BASE_DIR/ants/
-
 ant BMTTestGUI -f $BASE_DIR/ants/test/regressGUI.xml ^
 -Declipse.home=$ECLIPSE_HOME ^
 -Dtests.jar=$BASE_DIR/tests.jar ^
